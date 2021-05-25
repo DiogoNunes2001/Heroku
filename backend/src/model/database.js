8b9b78bb99b072edf5af1b79cbb980a86f41f596
Diogo Nunes <estgv18749@alunos.estgv.ipv.pt>
2021-05-26 00:05:59 +0100

    make it better

--- backend/src/model/database.js
+++ backend/src/model/database.js
@@ -1,7 +1,17 @@
 var Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("appAdmin", "postgres", "v7[_##H?9bQDK_PQ", { 
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  "dcr4clt2kc05pm",
+  "vzaomodcuhlwsp",
+  "e8e4702b202c0dad50af7b60f8ff92132ca173f2e605d4333502ef7d72bbef99",
+  {
+    port: "5432",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 module.exports = sequelize;
